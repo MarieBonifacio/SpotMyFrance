0887e5546a495f92fcb454134ea77c3d31fdc51d
ElodieBerquez <ElodieBerquez.pro@gmail.com>
2019-12-03 16:55:16 +0100

    diapo home ok

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -57,7 +57,7 @@ lien vers tt les lieux : <a href="{{route('place.index')}}">le lien</a> --}}
 
 
     <div class="carousel-cell">
-      <img src="img/p1.jpg" alt="paysage">
+      <img class="img" src="img/p1.jpg" alt="paysage">
 
       <div class="container_infos_lieu">
         <div class="title">
@@ -67,11 +67,22 @@ lien vers tt les lieux : <a href="{{route('place.index')}}">le lien</a> --}}
         <div class="text"> PARFAIT ! Pour les amateurs de sport en plein air, courrir sur le sable mouiller...
           Quel pied pour se remettre en forme ! ( surtout avec un week-end bien chargé !
           Nous avons profiter de ce spot en famille, les enfants, mon mari, haddock ( notre labrador) et moi-même Nous sommes en forme pour affronter notre semaine !</div>
+          <div class="share">
+            <p><i class="fas fa-plus-circle add"></i> Partagez vos photos</p>
+          </div>
+          <div class="commentaires">
+            <p>commentaires</p>
+            <div class="people">
+              <img class="picture" src="" alt="">
+              <p>Marie</p>
+              <p>Super lieu, moi aussi, j'y vais régulièrement.</p>
+            </div>
+          </div>
       </div>
     </div>
 
     <div class="carousel-cell">
-      <img src="img/p2.jpg" alt="paysage">
+      <img class="img" src="img/p2.jpg" alt="paysage">
 
       <div class="container_infos_lieu">
         <div class="title">
@@ -81,57 +92,67 @@ lien vers tt les lieux : <a href="{{route('place.index')}}">le lien</a> --}}
         <div class="text"> PARFAIT ! Pour les amateurs de sport en plein air, courrir sur le sable mouiller...
           Quel pied pour se remettre en forme ! ( surtout avec un week-end bien chargé !
           Nous avons profiter de ce spot en famille, les enfants, mon mari, haddock ( notre labrador) et moi-même Nous sommes en forme pour affronter notre semaine !</div>
+          <div class="share">
+            <p><i class="fas fa-plus-circle add"></i> Partagez vos photos</p>
+          </div>
+          <div class="commentaires">
+            <p>commentaires</p>
+            <div class="people">
+              <img class="picture" src="" alt="">
+              <p>Marie</p>
+              <p>Super lieu, moi aussi, j'y vais régulièrement.</p>
+            </div>
+          </div>
       </div>
     </div>
 
     <div class="carousel-cell">
-      <img src="img/p3.jpg" alt="paysage">
-      <div class="container_infos_lieu">
-        <div class="title">
-          <h1>Le titre du lieu</h1>
-          <p>La ville</p>
+        <img class="img" src="img/p4.jpg" alt="paysage">
+        <div class="container_infos_lieu">
+          <div class="title">
+            <h1>Le titre du lieu</h1>
+            <p>La ville</p>
+          </div>
+          <div class="text"> PARFAIT ! Pour les amateurs de sport en plein air, courrir sur le sable mouiller...
+            Quel pied pour se remettre en forme ! ( surtout avec un week-end bien chargé !
+            Nous avons profiter de ce spot en famille, les enfants, mon mari, haddock ( notre labrador) et moi-même Nous sommes en forme pour affronter notre semaine !</div>
+            <div class="share">
+              <p><i class="fas fa-plus-circle add"></i> Partagez vos photos</p>
+            </div>
+            <div class="commentaires">
+              <p>commentaires</p>
+              <div class="people">
+                <img class="picture" src="" alt="">
+                <p>Marie</p>
+                <p>Super lieu, moi aussi, j'y vais régulièrement.</p>
+              </div>
+            </div>
         </div>
-        <div class="text"> PARFAIT ! Pour les amateurs de sport en plein air, courrir sur le sable mouiller...
-          Quel pied pour se remettre en forme ! ( surtout avec un week-end bien chargé !
-          Nous avons profiter de ce spot en famille, les enfants, mon mari, haddock ( notre labrador) et moi-même Nous sommes en forme pour affronter notre semaine !</div>
       </div>
-    </div>
-    <div class="carousel-cell">
-      <img src="img/p4.jpg" alt="paysage">
-      <div class="text"> PARFAIT ! Pour les amateurs de sport en plein air, courrir sur le sable mouiller...
-        Quel pied pour se remettre en forme ! ( surtout avec un week-end bien chargé !
-        Nous avons profiter de ce spot en famille, les enfants, mon mari, haddock ( notre labrador) et moi-même Nous sommes en forme pour affronter notre semaine !</div>
-    </div>
-    <div class="carousel-cell">
-      <img src="img/p5.jpg" alt="paysage">
-      <div class="text"> PARFAIT ! Pour les amateurs de sport en plein air, courrir sur le sable mouiller...
-        Quel pied pour se remettre en forme ! ( surtout avec un week-end bien chargé !
-        Nous avons profiter de ce spot en famille, les enfants, mon mari, haddock ( notre labrador) et moi-même Nous sommes en forme pour affronter notre semaine !</div>
-    </div>
-    <div class="carousel-cell">
-      <img src="img/p6.jpg" alt="paysage">
-      <div class="text"> PARFAIT ! Pour les amateurs de sport en plein air, courrir sur le sable mouiller...
-        Quel pied pour se remettre en forme ! ( surtout avec un week-end bien chargé !
-        Nous avons profiter de ce spot en famille, les enfants, mon mari, haddock ( notre labrador) et moi-même Nous sommes en forme pour affronter notre semaine !</div>
-    </div>
-    <div class="carousel-cell">
-      <img src="img/p7.jpg" alt="paysage">
-      <div class="text"> PARFAIT ! Pour les amateurs de sport en plein air, courrir sur le sable mouiller...
-        Quel pied pour se remettre en forme ! ( surtout avec un week-end bien chargé !
-        Nous avons profiter de ce spot en famille, les enfants, mon mari, haddock ( notre labrador) et moi-même Nous sommes en forme pour affronter notre semaine !</div>
-    </div>
-    <div class="carousel-cell">
-      <img src="img/p8.jpg" alt="paysage">
-      <div class="text"> PARFAIT ! Pour les amateurs de sport en plein air, courrir sur le sable mouiller...
-        Quel pied pour se remettre en forme ! ( surtout avec un week-end bien chargé !
-        Nous avons profiter de ce spot en famille, les enfants, mon mari, haddock ( notre labrador) et moi-même Nous sommes en forme pour affronter notre semaine !</div>
-    </div>
-    <div class="carousel-cell">
-      <img src="img/p9.jpeg" alt="paysage">
-      <div class="text"> PARFAIT ! Pour les amateurs de sport en plein air, courrir sur le sable mouiller...
-        Quel pied pour se remettre en forme ! ( surtout avec un week-end bien chargé !
-        Nous avons profiter de ce spot en famille, les enfants, mon mari, haddock ( notre labrador) et moi-même Nous sommes en forme pour affronter notre semaine !</div>
-    </div>
+
+      <div class="carousel-cell">
+        <img class="img" src="img/p3.jpg" alt="paysage">
+        <div class="container_infos_lieu">
+          <div class="title">
+            <h1>Le titre du lieu</h1>
+            <p>La ville</p>
+          </div>
+          <div class="text"> PARFAIT ! Pour les amateurs de sport en plein air, courrir sur le sable mouiller...
+            Quel pied pour se remettre en forme ! ( surtout avec un week-end bien chargé !
+            Nous avons profiter de ce spot en famille, les enfants, mon mari, haddock ( notre labrador) et moi-même Nous sommes en forme pour affronter notre semaine !</div>
+            <div class="share">
+              <p><i class="fas fa-plus-circle add"></i> Partagez vos photos</p>
+            </div>
+            <div class="commentaires">
+              <p>commentaires</p>
+              <div class="people">
+                <img class="picture" src="" alt="">
+                <p>Marie</p>
+                <p>Super lieu, moi aussi, j'y vais régulièrement.</p>
+              </div>
+            </div>
+        </div>
+      </div>
   </div>
 
   <div class="carousel carousel-nav" data-flickity='{ "asNavFor": ".carousel-main", "contain": true, "pageDots": false}'>
@@ -145,26 +166,12 @@ lien vers tt les lieux : <a href="{{route('place.index')}}">le lien</a> --}}
       <img src="img/p2.jpg" alt="paysage">
     </div>
     <div class="carousel-cell">
-      <img src="img/p3.jpg" alt="paysage">
-    </div>
-    <div class="carousel-cell">
       <img src="img/p4.jpg" alt="paysage">
     </div>
     <div class="carousel-cell">
-      <img src="img/p5.jpg" alt="paysage">
+      <img src="img/p3.jpg" alt="paysage">
     </div>
-    <div class="carousel-cell">
-      <img src="img/p6.jpg" alt="paysage">
-    </div>
-    <div class="carousel-cell">
-      <img src="img/p7.jpg" alt="paysage">
-    </div>
-    <div class="carousel-cell">
-      <img src="img/p8.jpg" alt="paysage">
-    </div>
-    <div class="carousel-cell">
-      <img src="img/p9.jpeg" alt="paysage">
-    </div>
+ 
   </div>
 
 
